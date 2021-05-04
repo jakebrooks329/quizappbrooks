dca4d0649aa102d293307c98f0fdb7bdb38fd57d
Jake Brooks <70343237+jakebrooks329@users.noreply.github.com>
2021-05-04 09:13:37 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,97 @@
+const quizData = [
+    {
+        question: "Which year did 9/11 happen?",
+        a: "2004",
+        b: "2001",
+        c: "1999",
+        d: "2000",
+        correct: "b",
+    },
+    {
+        question: "What does SUV stand for?",
+        a: "Save Unintended Vets",
+        b: "Saturn, Uranus and Venus",
+        c: "Sport Utility Vehicle",
+        d: "Sperry, Untie, Velcrow",
+        correct: "c",
+    },
+    {
+        question: "What sports car company first made tractors?",
+        a: "Lamborghini",
+        b: "Ferrari",
+        c: "Porsche",
+        d: "Audi",
+        correct: "a",
+    },
+    {
+        question: "Who was the first president",
+        a: "James Madison",
+        b: "George Washington",
+        c: "John Adams",
+        d: "none of the above",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById('quiz')
+const answerEls = document.querySelectorAll('.answer')
+const questionEl = document.getElementById('question')
+const a_text = document.getElementById('a_text')
+const b_text = document.getElementById('b_text')
+const c_text = document.getElementById('c_text')
+const d_text = document.getElementById('d_text')
+const submitBtn = document.getElementById('submit')
+
+let currentQuiz = 0
+let score = 0
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers()
+
+    const currentQuizData = quizData[currentQuiz]
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function deselectAnswers() {
+    answerEls.forEach(answerEl => answerEl.checked = false)
+}
+
+function getSelected() {
+    let answer
+
+    answerEls.forEach(answerEl => {
+        if(answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+submitBtn.addEventListener('click', () => {
+    const answer = getSelected()
+    
+    if(answer) {
+        if(answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+
+        if(currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered ${score}/${quizData.length} questions correctly</h2>
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
